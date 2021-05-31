97395095d1a3cf67ee4250dbc4fad58a802da779
unknowns <unknowns0074@gmail.com>
2021-05-31 16:33:30 -0300

    Se añadieron middlewares importantes como el recover y el logger

--- src/main.go
+++ src/main.go
@@ -5,11 +5,18 @@ import (
 	"app_template/src/routes"
 	"app_template/src/utils"
 	"fmt"
+	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+
+	/////////////////////////////
+	// Loading the config data //
+	/////////////////////////////
+
 	config, err := utils.LoadConfig(".")
 
 	if err != nil {
@@ -31,13 +38,42 @@ func main() {
 		Print_log:    config.DBLog,
 	}
 
+	// Opening the database connection
 	libs.DB = dbCfg.InitMysqlDB()
 
+	// Initializing web app
 	app := echo.New()
+
+	//////////////////////////////////
+	// Using importants middlewares //
+	//////////////////////////////////
+
+	// Recover middleware
+	app.Use(middleware.Recover())
+
+	// Logging middleware
+	myLogFile, err := os.OpenFile("./src/logs/requests.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("FATAL ERROR!!!\n Cannot open or create log file:", err)
+		return
+	}
+	defer myLogFile.Close()
+
+	app.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Output: myLogFile,
+	}))
+
+	///////////////////
+	// Adding routes //
+	///////////////////
 
 	routes.CreateAuthRoutes(app) // Create login, logoute, register, verificate Routes
 	routes.CreateWebRoutes(app)  // Create web routes f.e. /, contact
 	routes.CreateApiRoutes(app)  // create api routes
+
+	//////////////////
+	// Starting app //
+	//////////////////
 
 	app.Logger.Fatal(app.Start(config.APPPort))
 }
--- src/routes/web.go
+++ src/routes/web.go
@@ -1,11 +1,9 @@
 package routes
 
 import (
-	controllers "app_template/src/app/Http/controllers/user"
-
 	"github.com/labstack/echo/v4"
 )
 
 func CreateWebRoutes(app *echo.Echo) {
-	app.GET("/", controllers.UserController.Add)
+	//app.GET("/", controllers.UserController.Add)
 }
